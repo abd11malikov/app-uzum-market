4ac4052c66672991b7e0343ca89a51447333b074
muhammadvali08 <muhammadvalimoydinov0808@gmail.com>
2023-05-18 17:09:16 +0500

    asdfghasd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 class Main{
     public static void main(String[] args) {
         System.out.println("salom bachalar");
+
         System.out.println("salom bachalar12345");
         System.out.println("salom bachalar12345");
 
