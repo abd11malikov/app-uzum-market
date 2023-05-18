303442a077e7db7ce14b386d5b712323cd0e6576
abd11malikov <otabekabd11malikov@gmail.com>
2023-05-18 17:20:30 +0500

    rtyereeqw

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("ksjahkdhas");
         System.out.println("umud");
+
         System.out.println("umid nima gap tizza");
     }
 }
