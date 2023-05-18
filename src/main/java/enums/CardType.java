bf89374e9752dd6243d8c54bbe1542703598c42e
AAA <ulugbekturgunov0423@gmail.com>
2023-05-18 18:03:41 +0500

    ssadafag

--- src/main/java/enums/CardType.java
+++ src/main/java/enums/CardType.java
@@ -5,5 +5,15 @@ package enums;
 public enum CardType {
     XUMO,
     UZCARD,
-    VIZA
+    VIZA;
+
+    public CardType cardType (String cardType) {
+        for (CardType cardType1 : values()) {
+            if (cardType1.name().equals(cardType)) {
+                return cardType1;
+            }
+        }
+        return null;
+    }
+
 }
