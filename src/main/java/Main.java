c914828f1e0b4267b707b796762ad7f1456d15ea
AAA <ulugbekturgunov0423@gmail.com>
2023-05-18 16:53:57 +0500

    umid nima gap

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args) {
         System.out.println("ksjahkdhas");
         System.out.println("umud");
+        System.out.println("umid nima gap tizza");
     }
 }
