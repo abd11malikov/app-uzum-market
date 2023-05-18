20af6a841d6e135ba0534cfc7354e5308bb3bd41
abd11malikov <otabekabd11malikov@gmail.com>
2023-05-18 17:34:17 +0500

    rtyereeqwgg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("salom bachalar");
-        System.out.println("salom bachalar12345");
-
-        System.out.println("salom bachalar12345");
+        System.out.println("serquyosh");
     }
 }
