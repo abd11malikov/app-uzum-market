b459758bb30fd0fe91ed829e3372d72b5d4c1da7
muhammadvali08 <muhammadvalimoydinov0808@gmail.com>
2023-05-18 18:03:49 +0500

    sdf

--- src/main/java/enums/Role.java
+++ src/main/java/enums/Role.java
@@ -1,4 +1,17 @@
 package enums;
 
 public enum Role {
+    OWNER,
+    ADMIN,
+    USER,
+    PRODUCT_OWNER;
+
+    public Role getRole(String roleName){
+        for(Role role : values()){
+         if(role.name().equals(roleName)){
+             return role;
+         }
+        }
+        return null;
+    }
 }
