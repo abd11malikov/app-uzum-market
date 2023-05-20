d05472b311458252efc89d2db3e0e4576bead0c5
muhammadvali08 <muhammadvalimoydinov0808@gmail.com>
2023-05-20 18:10:37 +0500

    akang

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -1,4 +1,5 @@
 public interface Constant {
-    String ENTRANCE = "1.Sign up\t\t2.Sign in\t\t0.Exit";
+    String ENTRANCE = "1.Sign up\t\t2.Sign in\t\t3.Category\t\t0.Exit";
+    String WRONG_OPTION = "Wrong option !";
 
 }
