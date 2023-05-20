ed4d5295d69490b26da26fb6135a0a8ca0d81954
abd11malikov <oabdumalikov912@gmail.com>
2023-05-20 18:09:11 +0500

    sas

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -1,5 +1,10 @@
 public interface Constant {
     String ENTRANCE = "1.Sign up\t\t2.Sign in\t\t0.Exit";
-
-
+    String WRONG_OPTION = "Wrong option";
+    String OWNER_PAGE = "1.Admin\t\t2.User\t\t3.Category\t\t0.Exit";
+    String ADMIN_FRONT= "1.Admin list\t\t2.Add admin\t\t3.Delete admin\t\t0.Exit";
+    String USER_FRONT = "1.User list\t\t2.Add user\t\t3.Delete user\t\t0.Exit";
+    String CATEGORY_FRONT = "1.Category list\t\t2.Add category\t\t3.Delete category\t\t4.Edit category\t\t0.Exit";
+    String SUBCATEGORY_FRONT = "1.Subcategory list\t\t2.Add subcategory\t\t3.Delete subcategory\t\t4.Edit subcategory\t\t0.Exit";
+    String PRODUCT_FRONT = "1.Product list\t\t2.Add product\t\t3.Delete product\t\t4.Edit product\t\t0.Exit";
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,57 @@
-class Main{
-    public static void main(String[] args) {
+import enums.Gender;
+import model.User;
+import service.user.UserService;
+import service.user.UserServiceImp;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+
+
+class Main implements Constant{
+    static Scanner scannerInt = new Scanner(System.in);
+    static Scanner scannerStr = new Scanner(System.in);
+    static UserService userService = new UserServiceImp();
+
+    public static void main(String[] args) throws SQLException {
+        while (true){
+            System.out.println(ENTRANCE);
+            int option = scannerInt.nextInt();
+            if (option==1) signUp();
+            else if (option==2) signIn();
+            else if (option == 3) category();
+            else if (option==0) break;
+            else System.err.println(WRONG_OPTION+"\n");
+        }
+    }
+
+    private static void category() {
+    }
+
+    private static void signIn() {
+    }
+
+    private static void signUp() throws SQLException {
+        System.out.print("Enter number : ");
+        String phone = scannerStr.nextLine();
+        System.out.print("Enter name : ");
+        String last = scannerStr.nextLine();
+        System.out.print("Choose gender [1.Male 2.Female]: ");
+        String gender;
+        int option = scannerInt.nextInt();
+        if (option== 1) gender = "MALE";
+        else if(option == 2) gender = "FEMALE";
+        else {System.err.println(WRONG_OPTION+"\n");
+            return;
+        }
+        User user = User.builder()
+                .name(last)
+                .phoneNumber(phone)
+                .gender(Gender.valueOf(gender))
+                .build();
+        int i = userService.create(user);
+        if (i>0) System.out.println("User created successfully");
+        else System.err.println("User not created\n");
     }
 }
